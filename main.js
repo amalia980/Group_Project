d7e967e23fce2f94a72cb3fea2709e73b3f6f62e
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2021-06-04 20:28:00 +0200

    third push

--- main.js
+++ main.js
@@ -1,75 +1,59 @@
-//Elements needed
-const form = document.getElementById("form");
-const whenClicked = document.getElementById("button");
-const showLyrics = document.getElementById("lyrics");
-const errorMessage = document.getElementById("error");
+//get API, data is in objects
 
+//info about the API. parameters are "artist" and "title". "lyrics" is the 
+/*
+function URL(artist, title) {
+  return `https://api.lyrics.ovh/v1/${artist}/${title}`;
+}
+
+function showLyrics(artist, title) {
+  const geturl = URL(artist, title);
+
+  fetch(geturl).then(function(response){
+      response.json().then(function(data) {
+
+      });
+  });
+}
+  
+showLyrics();
+*/
+
+//HTML elements needed
+//const form = document.getElementById("form");
+const whenClicked = document.getElementById("button");//button
+const showLyrics = document.getElementById("lyrics");//create textarea inside this
+const errorMessage = document.getElementById("error");//show error message
+const songTitle = document.getElementById("song");//inputfield song
+const artistName = document.getElementById("artist");//inputfield artist
+
+function getLyrics(artist, title) {
+  fetch("https://api.lyrics.ovh/v1/")
+  .then(response => response.json())
+  .then(lyrics => {
+    showLyrics.innerHTML = `<textarea ${lyrics.file}></textare>`
+  });
+}
+
+getLyrics(artist.value, title.value);
+
+//create textarea to display results(the lyrics) in
+const textArea = document.createElement("textarea");
+textArea.appendChild(lyrics); //<textarea> inside <div id="lyrics"</div>
+
+//when button is clicked
 whenClicked.addEventListener("click", function() {
 
-  const songTitle = document.getElementById("song");
-  const stringValueTitle = songTitle.value;
-  const stringLengthTitle = stringValueTitle.length;
+  const artistValue = songTitle.value;
+  const artistLength = artistValue.length;
 
-  const artistName = document.getElementById("artist");
-  const stringValueSong = artistName.value;
-  const stringLengthSong = stringValueSong;
+  const songValue = artistName.value;
+  const songLength = songValue;
 
-  if(stringLengthTitle && stringLengthSong === ) {
+  if(artistLength && songLength === showLyrics) {
     showLyrics.innerHTML;
   }
   else {
       errorMessage.innerText = "Please fill in correct information";
   }
-
-}
-
-/*
-  function URL(artist, song) {
-    return `https://api.lyrics.ovh/v1/${artist}/${song}`;
-}
-
-function showLyrics(artist, song) {
-    const geturl = URL(artist, song);
-
-    fetch(geturl).then(function(response){
-        response.json().then(function(data) {
-            console.log(data.lyrics);
-        });
-    });
-}
-*/
 });
-
-  
-/*
-  const results = [ {
-    artist: "",
-    song: ""
-    },
-
-    {
-    artist: "",
-    song: ""
-    },
-];
-*/
-  //Array with the data
-
-  
-
-  //Function for searching lyrics
-/*
-  function search() => results.filter {
-      if() {
-
-      }
-      else {
-          errorMessage.innerText = "Please fill in correct information";
-      }
-
-  }
-
-  const found = (search);
-  
-  console.log(found);
-*/
